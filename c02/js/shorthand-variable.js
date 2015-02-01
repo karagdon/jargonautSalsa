e052592345864f5dccae2fa3cb6945b9e3b94b27
karagdon <karagdon@gmail.com>
2015-01-31 21:40:31 -0800

    daily code

---
+++ c02/js/shorthand-variable.js
@@ -0,0 +1,17 @@
+var price = 5;
+var quantity = 14;
+var total = price * quantity;
+
+var price, quantity, total;
+price = 5;
+quantity = 14;
+total = price * quantity
+
+var price = 5, quantity = 14;
+var total = price * quantity;
+
+// Write total into the element with the id of cost
+var el = docuement.getElementbyId('cost');
+el.textContent = '$' + total;
+
+
