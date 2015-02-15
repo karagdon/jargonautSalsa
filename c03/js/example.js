eb8b59792bc4b36032a89f770594487d60fd6f26
karagdon <karagdon@gmail.com>
2015-02-14 22:43:43 -0800

    daily code

---
+++ c03/js/example.js
@@ -0,0 +1,23 @@
+(function() {
+	// CREATE A HOTEL OBJECT AND WRITE OUT THE OFFER DETIALS
+
+	var hotel = {
+		name: 'Park',
+		roomRate: 240, // in dollars
+		discount 15, 	// Percent
+		offerPrice: function() {
+			var offerRate = this.roomRate * ((100 - this.discount) / 100);
+			return offerRate
+		}
+	}
+
+// Write out the hotel name, standard rate, and special rate
+var hotelName, roomRate, specialRate;
+hotelName = document.getElementById('hotelName');
+roomName = document.getElementById('roomName');
+speciallName = document.getElementById('speciallName');
+
+hotelName.textContent = hotel.name;
+roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+specialRate.textContent = '$' + hotel.offerPrice();
+})
