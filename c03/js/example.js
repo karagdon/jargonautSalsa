d3c22cd939882ac8b7d0b42ff51bdfeaf6605433
karagdon <karagdon@gmail.com>
2015-02-15 23:26:47 -0800

    daily code

--- c03/js/example.js
+++ c03/js/example.js
@@ -20,4 +20,26 @@ speciallName = document.getElementById('specialName');
 hotelName.textContent = hotel.name;
 roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
 specialRate.textContent = '$' + hotel.offerPrice();
-})
+
+
+// CALCULATE AND WRITE OUT THE EXPIRY DETAILS FOR THE OFFER
+var expiryMsg
+var today;
+var elEnds;
+
+function offerEnds(today) {
+	// declare variables within the function for local scope
+	var weekFromToday, day, date, month, year, dayName, monthNames;
+	// Add 7 days time (add in milliseconds)
+	weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+	dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+	monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December']
+	day = dayNames[weekFromToday.getDay()];
+	date = weekFromToday.getDate();
+	month = monthNames[weekFromToday.getMonth()];
+	year = weekFromToday.getFullYear();
+	expiryMsg = 'Offer expires next ';
+	expiryMsg += day ' <br />(' + date + ' '+ month + ' '+ year + ')';
+	return expiryMsg;
+}
+}());
