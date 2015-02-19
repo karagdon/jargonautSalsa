4c3ec6354a936785db0b4e272e461a94a7852c2a
karagdon <karagdon@gmail.com>
2015-02-18 23:15:40 -0800

    daily code

--- c04/js/comparison-operator-continued.js
+++ c04/js/comparison-operator-continued.js
@@ -5,6 +5,5 @@ var highScore2 = 95;
 
 var comparison= (score1 + score2) > (highScorel + highScore2);
 
-
 var el = document.getElementById('answer');
 el.textContent ='New high score:'+ comparison;
