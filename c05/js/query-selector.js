81fa6b28594ee4d16897b88dfeb192d27d03830a
karagdon <karagdon@gmail.com>
2015-02-27 23:01:23 -0800

    daily code

---
+++ c05/js/query-selector.js
@@ -0,0 +1,9 @@
+// querySelector()only returns the first match
+var el = document.querySelector('li.hot');
+el.className = 'cool';
+
+//query selectorAll returns a NodeList
+//The second matching element (the third list item ) is selected and changed
+
+var els = document.querySelectorAll('li.hot')
+els[1].className = 'cool';
