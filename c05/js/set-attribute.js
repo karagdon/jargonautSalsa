e8aaf838f0407be0cce387e32bcb2a24ba47aad1
karagdon <karagdon@gmail.com>
2015-03-04 22:27:29 -0800

    daily code

---
+++ c05/js/set-attribute.js
@@ -0,0 +1,11 @@
+// Get the first item
+var firstItem = document.getElementById('one');
+
+// Change its class attribute
+firstItem.className = 'complete';
+
+// get fourth item
+var fourthItem = document.getElementsByTagName('li').item(3);
+
+// add an attribute to it
+fourthItem.setAttribute('class','cool');
