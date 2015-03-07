ba937937fd782dc5d6c275583efacc5bc3ee5a71
karagdon <karagdon@gmail.com>
2015-03-06 21:38:32 -0800

    daily code

---
+++ c06/js/event-listener-with-ie-fallback.js
@@ -0,0 +1,17 @@
+var elUsername = document.getElementById('username');
+var elMsg = document.getElementById('feedback');
+
+function checkUsername(minLength) {
+	if (elUsername.value.length < minLength ) {
+	// Set Msg
+		elMsg.innerHTML = 'username must be ' + minLength + ' characters or more';
+	} else {
+	elMsg.innerHTML = '';
+	}
+}
+if (elUsername.addEventListener) {
+	elUsername.addEventListener('blur', function(){checkUsername(5);}, false);
+}else {
+	//IE fallback: onblur
+	elUsername.attachEvent('onblur',function(){checkUsername(5);});
+}
