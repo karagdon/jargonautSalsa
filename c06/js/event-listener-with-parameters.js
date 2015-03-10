1d34577da905292f423ec8376cd96fad125d03f7
karagdon <karagdon@gmail.com>
2015-03-09 23:03:14 -0700

    daily code

--- c06/js/event-listener-with-parameters.js
+++ c06/js/event-listener-with-parameters.js
@@ -2,7 +2,7 @@ var elUsername = document.getElementById('username');
 var elMsg = document.getElementById('feedback');
 
 function checkUsername(minLength){
-	if (elUsername.value.length < 5) {
+	if (elUsername.value.length < minLength) {
 		elMsg.textContent = 'Username must be ' + minLength + ' characters or more';
 	}else {
 		elMsg.textContent = '';
