c2d867646cc67af1b66ba3cc4d60ba2ff0cd2b3e
karagdon <karagdon@gmail.com>
2015-03-15 21:43:12 -0700

    daily code

--- c06/js/keypress.js
+++ c06/js/keypress.js
@@ -1,14 +1,14 @@
-var el;                                                    // Declare variables
-
-function charCount(e) {                                    // Declare function
-  var textEntered, charDisplay, counter, lastkey;          // Declare variables
-  textEntered = document.getElementById('message').value;  // User's text
-  charDisplay = document.getElementById('charactersLeft'); // Counter element
-  counter = (180 - (textEntered.length));                  // Num of chars left
-  charDisplay.textContent = counter;                       // Show chars left
-
-  lastkey = document.getElementById('lastKey');            // Get last key used
-  lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode; // Create msg 
+var el;
+function charCount(e){
+	var textEntered, charDisplay, counter, library;
+	textEntered = document.getElementById('message').value;
+	charDisplay = document.getElementById('characters left');
+	counter = (180 - (textEntered.length));
+	charDisplay.textContent = counter;
+	
+	lastkey = document.getElementById('lastkey');
+	lastkey.textContent = 'Last key in ASCII code: ' + e.keyCode;
 }
-el = document.getElementById('message');                   // Get msg element
-el.addEventListener('keypress', charCount, false);// keypress -call charCount()
+
+el = document.getElementById('message');
+el.addEventListener('keypress', charCount, false);
