46976a988cc9476b2c978ae0683bf8a0d5e1649b
karagdon <karagdon@gmail.com>
2015-03-18 22:50:28 -0700

    daily code

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -1,8 +1,9 @@
-var elList, addLink, newEl, newText, counter, listitems;
+var elList, addLink, addClass, newEl, newText, counter, listitems;
 
 elList = document.getElementById('list');
 addLink = document.querySelector('a');
 counter = document.getElementById('counter');
+addClass = document.querySelector('li');
 
 function addItem(e) {
 	e.preventDefault();
@@ -10,11 +11,19 @@ function addItem(e) {
 	newText = document.createTextNode('New list item');
 	newEl.appendChild(newText);
 	elList.appendChild(newEl);
+
 }
 
 function updateCount() {
 	listitems = list.getElementsByTagName('li').length;
 	counter.innerHTML = listitems;
+}
+
+function updateClass() {
+ 	// change the class for msg, add the msg
+ 	if (newEl.className == 'warm')
+ 		{newEl.className == 'cool';}
+ 		else{newEl.className = 'warm';}
 }
 
 addLink.addEventListener('click', addItem, false);
