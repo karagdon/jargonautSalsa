35755221af29d56371b16f72cceae7f26e518880
karagdon <karagdon@gmail.com>
2015-03-21 22:43:43 -0700

    daily code

--- c06/js/example.js
+++ c06/js/example.js
@@ -7,16 +7,18 @@ function writeLabel(e)
 target = event.target || event.srcElement;
 textEntered = e.target.value;
 noteName.textContent = textEntered;
-
-if (document.addEventListener)
-	{
-		document.addEventListener('click', function(e){recorderControls(e);}, false);
-		username.addEventListener('input', writeLabel, false);
-	}else{
-		docuement.attachEvent('onclick', function(e){recorderControls(e);} );
-		username.attachEvent('onkeyup', writeLabel, false);
+}
+// if (document.addEventListener)
+// 	{
+// 		document.addEventListener('click', function(e) {
+// 			recorderControls(e);
+// 		}, false);
+// 		username.addEventListener('input', writeLabel, false);
+// 	}else{
+// 		document.attachEvent('onclick', function(e){recorderControls(e);}, false );
+// 		username.attachEvent('onkeyup', writeLabel, false);
 		
-	}
+// 	}
 function recorderControls(e)
 	{if (!e){e = window.event;}}
 	target = event.target || event.srcElement;
@@ -37,7 +39,6 @@ function record(target){
 	target.setAttribute('data-state', 'stop');
 	target.textContent = 'stop';
 }
-
 function stop(target){
 	target.setAttribute('data-state', 'record');
 	target.textContent = 'record';
