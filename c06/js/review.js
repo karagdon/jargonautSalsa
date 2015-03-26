de5b5c7d04c78387349f3987cce916b6754e65bc
karagdon <karagdon@gmail.com>
2015-03-25 22:30:10 -0700

    daily code

--- c06/js/review.js
+++ c06/js/review.js
@@ -5,7 +5,7 @@ addLink = document.getElementById('addLast');
 counter = document.getElementById('counter');
 addClass = document.querySelector('li');
 addLinkFirst = document.getElementById('addFirst');
-
+addClass.setAttribute('onclick', 'updateClass();');
 
 function addFirst(e) {
 	e.preventDefault();
@@ -13,6 +13,7 @@ function addFirst(e) {
 	newItem = document.getElementById('newItemInput').value;
 	newText = document.createTextNode(newItem);
 	newEl.appendChild(newText);
+	newEl.className = 'cool';
 	elList.appendChild(newEl);
 	var list = document.getElementsByTagName('ul')[0];
 	list.insertBefore(newEl, list.firstChild);
@@ -21,25 +22,33 @@ function addFirst(e) {
 function addItem(e) {
 	e.preventDefault();
 	newEl = document.createElement('li');
+	
 	newItem = document.getElementById('newItemInput').value;
 	newText = document.createTextNode(newItem);
 	newEl.appendChild(newText);
+	newEl.className = 'hot';
 	elList.appendChild(newEl);
 }
+
 function updateCount() {
 	listitems = list.getElementsByTagName('li').length;
 	counter.innerHTML = listitems;
 }
 
-// function updateClass() {
-//  	// change the class for msg, add the msg
-//  	if (newEl.className == 'warm')
-//  		{newEl.className == 'cool';}
-//  		else{newEl.className = 'warm';}
-// }
+function updateClass() {
+ 	// change the class for msg, add the msg
+ 	if (addClass.className == 'warm')
+ 	
+ 		{addClass.className == 'hot';
+ 			if (addClass.className == 'cool')
+ 			{addClass.className == 'hot';}
+ 			
+ 		}else{addClass.className = 'cool';}
+		
+}
 
-
+addClass.addEventListener('onclick', updateClass, false);
 addLink.addEventListener('click', addItem, false);
 addLinkFirst.addEventListener('click', addFirst, false);
 elList.addEventListener('DOMNodeInserted', updateCount, false);
-// status.addEventListener('click', updateClass, false);
+
