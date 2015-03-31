f6ea7ab916fb299323e64a9f615900608bf9258e
karagdon <karagdon@gmail.com>
2015-03-30 23:34:08 -0700

    daily code

--- c06/js/review.js
+++ c06/js/review.js
@@ -13,7 +13,7 @@ function addFirst(e) {
 	newItem = document.getElementById('newItemInput').value;
 	newText = document.createTextNode(newItem);
 	newEl.appendChild(newText);
-	newEl.setAttribute('onclick', 'updateClass()');
+	newEl.setAttribute('onclick', 'li-task');
 	newEl.className = 'cool';
 	elList.appendChild(newEl);
 	var list = document.getElementsByTagName('ul')[0];
@@ -37,60 +37,56 @@ function updateCount() {
 }
 
 function updateListwithClass() {
-	newEl.setAttribute('onclick', 'updateClass()');
+	newEl.setAttribute('onclick', 'updateClass');
 }
 
 
-//change code to work with this instead of below
-$("li.task").mouseover(function () {
-    $(this).addClass("hovered-task");
-});
-
-$("li.task").mouseout(function () {
-    $(this).removeClass("unhovered-task");
-});
-
-
-$("li.task").click(function() {
- 	if ($(this).className === '')
- 		{$(this).addClass('hot');}
- 		
-	else if ($(this).className == 'hot')
- 		{
- 			$(this).removeClass('hot');
- 			$(this).addClass('cold');
- 		}
- 		else if ($(this).className == 'cold')
- 		{
- 			$(this).removeClass('cold');
- 			$(this).addClass('warm');
- 		}
-  		else if ($(this).className == 'warm')
- 		{
- 			$(this).removeClass('warm');
- 			$(this).addClass('hot');
- 		}
-})
-
-
-// function updateClass() {
-//  	//change the class for msg, add the msg
-//  	if (addClass.className === '')
-//  		{addClass.className = 'hot';}
-// 	else if (addClass.className == 'hot')
-//  		{addClass.className = 'cool';}
-// 	else if (addClass.className == 'cool')
-//  		{addClass.className = 'warm';}
-// 	else if (addClass.className == 'warm')
-//  		{addClass.className = 'hot';}
+////change code to work with this instead of below
+//$("li-task").mouseover(function () {
+//    $(this).addClass("hovered-task");
+//});
+//
+//$("li-task").mouseout(function () {
+//    $(this).removeClass("unhovered-task");
+//});
+//
+//
+//$("li-task").click(function() {
+// 	if ($(this).className === '')
+// 		{$(this).addClass('hot');}
+//
+//	else if ($(this).className == 'hot')
+// 		{
+// 			$(this).removeClass('hot');
+// 			$(this).addClass('cold');
+// 		}
+// 		else if ($(this).className == 'cold')
+// 		{
+// 			$(this).removeClass('cold');
+// 			$(this).addClass('warm');
+// 		}
+//  		else if ($(this).className == 'warm')
+// 		{
+// 			$(this).removeClass('warm');
+// 			$(this).addClass('hot');
+// 		}
+//})
 
 
-
-
+function updateClass() {
+ 	//change the class for msg, add the msg
+ 	if (addClass.className === '')
+ 		{addClass.className = 'hot';}
+	else if (addClass.className == 'hot')
+ 		{addClass.className = 'cool';}
+	else if (addClass.className == 'cool')
+ 		{addClass.className = 'warm';}
+	else if (addClass.className == 'warm')
+ 		{addClass.className = 'hot';}	}
 
 addClass.addEventListener('onclick', updateClass(), false);
+
 addLink.addEventListener('click', addItem, false);
 addLinkFirst.addEventListener('click', addFirst, false);
 elList.addEventListener('DOMNodeInserted', updateCount, false);
 newEl.addEventListener('DOMNodeInserted', updateListwithClass, false);
-
