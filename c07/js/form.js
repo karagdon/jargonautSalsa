f1c64e6092eb909db577425197386d904fa6a502
karagdon <karagdon@gmail.com>
2015-04-09 22:57:38 -0700

    daily code

--- c07/js/form.js
+++ c07/js/form.js
@@ -3,20 +3,20 @@ $(function(){
 	var $newItemForm = $('#newItemForm');
 	var $textInput = $('input:text');
 
-	$newItemButton.show();
-	$newItemForm.hide();
+	// $newItemButton.show();
+	// $newItemForm.hide();
 	
-	$('#showForm').on('click', function(){
-		$newItemButton.hide();
-		$newItemForm.show();
-	})
+	// $('#showForm').on('click', function(){
+	// 	$newItemButton.hide();
+	// 	$newItemForm.show();
+	// })
 	
 	$newItemForm.on('submit', function(e){
 		e.preventDefault();
 		var newText = $('input:text').val();
 		$('li:last').after('<li>' + newText + '</li>');
-		$newItemForm.hide();
-		$newItemButton.show();
+		// $newItemForm.hide();
+		// $newItemButton.show();
 		$textInput.val('');
 	});
 	
