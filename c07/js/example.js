97e11c9a4c1aa36044e5be8d87b4ac04ed2b657f
karagdon <karagdon@gmail.com>
2015-04-12 23:52:19 -0700

    daily code

---
+++ c07/js/example.js
@@ -0,0 +1,28 @@
+// Practice Event delegation
+
+// TODO: users can add new list items
+// TODO: Click to indicate an item is <status>
+// TODO: Once complete, another <click> will ..
+		// if 'Not complete', will 1) move to bottom 2) mark as 'complete'
+		// if 'complete', will 1) fade, 2) remove from list, 3) update counter
+// TODO: Updated count of the number of items there are in the list, shown at heading <h2></h2>
+
+
+$(function(){
+// shorthand for document.ready();
+	
+// variables here
+
+
+// UI elements
+// List
+// Button
+
+// Add a new list item
+// Remove a new list item
+	// Fade effect
+	// Move to the bottom of the list
+	
+// update the counter
+
+};)
