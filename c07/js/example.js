9f9e71c03ee44677da0b3da724ca593c35536247
karagdon <karagdon@gmail.com>
2015-04-16 23:57:47 -0700

    daily code

--- c07/js/example.js
+++ c07/js/example.js
@@ -1,53 +1,39 @@
 $(function() {
+
+  // SETUP
   var $list, $newItemForm, $newItemButton;
   var item = '';
   $list = $('ul');
   $newItemForm = $('#newItemForm');
-  $newItemButton = $('#newItemButton');
-
+  
+  // ITEM COUNTER
   function updateCount() {
     var items = $('li[class!=complete]').length;
     $('#counter').text(items);
   }
   updateCount();
 
-
-  // $newItemButton.show();
-  $newItemForm.show();
-  // $('#showForm').on('click', function() {
-  //   $newItemButton.hide();
-  //   $newItemForm.show();
-  // });
-
   // ADDING A NEW LIST ITEM
   $newItemForm.on('submit', function(e) {
     e.preventDefault();
     var text = $('input:text').val();
-    $list.append('<li>' + text + '</li>');
+    $list.append('<li>' + text + '</li>').delay(450 * index).fadeIn(1600);
     $('input:text').val('');
     updateCount();
   });
+});
+
+// $( "li" ).click(function() {
+//   $(this).toggleClass("warm cool complete hot");
+// });
 
 
-  $list.on('click', 'li', function() {
-    var $this = $(this);
-    var complete = $this.hasClass('complete');
-
-    if (complete === true) {
-      $this.animate({
-        opacity: 0.0,
-        paddingLeft: '+=180'
-      }, 500, 'swing', function() {
-        $this.remove();
-      });
-    } else {
-      item = $this.text();
-      $this.remove();
-      $list
-        .append('<li class=\"complete\">' + item + '</li>')
-        .hide().fadeIn(300);
-      updateCount();
-    }
+var cls = [ "cool", "warm", "hot", "complete" ];
+var uls = $( "ul" ).children();
+$( "li" ).on( "click", function( event ) {
+  event.preventDefault();
+  this.each(function( i ) {
+    this.className = cls[ i ];
   });
-
+  appendClass();
 });
