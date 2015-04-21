d96e93ab3b3efac3e4112464a8a0e7e939684567
karagdon <karagdon@gmail.com>
2015-04-21 00:19:38 -0700

    daily code

--- c07/js/example.js
+++ c07/js/example.js
@@ -1,56 +1,56 @@
-$( document ).ready(function() {
- updateCount();
+   updateCount();
 
-  // SETUP
-  var $list, $newItemForm, $newItemButton;
-  var item = '';
-  $list = $('ul');
-  $newItemForm = $('#newItemForm');
+  //SETUP
+    var $list, $newItemForm, $newItemButton;
+    var item = '';
+    $list = $('ul');
+    $newItemForm = $('#newItemForm');
+
   
   // ADDING A NEW LIST ITEM
   $newItemForm.on('submit', function(e) {
     e.preventDefault();
     var text = $('input:text').val();
-    $list.append('<li class="warm">' + text + '</li>');
+    $list.append('<li class="cool">' + text + '</li>');
     $('input:text').val('');
     updateCount();
   });
-
-
-$('li').click(function () {
-  // TODO: change class of new list items
-  // change the class
-  var classes = ['hot','warm','cool', 'complete'];
-  $(this).each(function(){
-    this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+// TODO: change class of new list items
+  $('li').click(function () {
+    
+    // change the class
+    var classes = ['hot','warm','cool', 'complete'];
+      $(this).each(function(){
+        this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
+      });
+    
+    //Update the counter
+    updateCount();
+      
   });
+
+
   
-  //Update the counter
-  updateCount();
-  
-});
-classes
   // ITEM COUNTER
   function updateCount() {
     var itemsAll = $('li[class!=complete]').length;
     $('#counter').text(itemsAll);
+      
+    // COOL ITEM COUNTER
+      var itemsHot = $('li[class=hot]').length;
+      $('#counterHot').text(itemsHot);
+  
     
-  // COOL ITEM COUNTER
-    var itemsHot = $('li[class=hot]').length;
-    $('#counterHot').text(itemsHot);
-
+    // WARM ITEM COUNTER
+      var itemsWarm = $('li[class=warm]').length;
+      $('#counterWarm').text(itemsWarm);
   
-  // WARM ITEM COUNTER
-    var itemsWarm = $('li[class=warm]').length;
-    $('#counterWarm').text(itemsWarm);
-
+    
+    // HOT ITEM COUNTER
+      var itemsCool = $('li[class=cool]').length;
+      $('#counterCool').text(itemsCool);
   
-  // HOT ITEM COUNTER
-    var itemsCool = $('li[class=cool]').length;
-    $('#counterCool').text(itemsCool);
-
-  // COMPLETE ITEM COUNTER
-    var itemsDone = $('li[class=complete]').length;
-    $('#counterComplete').text(itemsDone);
+    // COMPLETE ITEM COUNTER
+      var itemsDone = $('li[class=complete]').length;
+      $('#counterComplete').text(itemsDone);
   }
-});
