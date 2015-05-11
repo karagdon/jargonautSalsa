c842745ce88d4c05e7971c4add6bfd9e7869a2de
karagdon <karagdon@gmail.com>
2015-05-10 23:55:53 -0700

    daily code

--- c08/js/example.js
+++ c08/js/example.js
@@ -1,9 +1,16 @@
-$(#content).on('click', '#sessions il a', function(e){
+// Click on a session to load the description
+
+$('#content').on('click', '#sessions il a', function(e){
   e.preventDefault();
   var fragment = this.href;
 
   fragment = fragment.replace('#', ' #');
   $('#details').load(fragment);
-  
+
+  // Update selected snippet
+  $('#sessions a.current').removeClass('current');
+  $('this').addClass('current');
 });
 
+
+// Click on primary navigation
