ffd93f7798aaa332b5d27225b74f4f220c8a42ce
karagdon <karagdon@gmail.com>
2015-05-14 22:21:21 -0700

    daily code

--- c08/js/example.js
+++ c08/js/example.js
@@ -10,11 +10,24 @@ $(function() {
   })
 }
 function loadTimeTable(){
-  $.getJSON('data/example.json').done(function(data){times = data;}).fail(function(){$('event').html("Sorry!");
+  $.getJSON('data/example.json').done(function(data){times = data;}).fail(function(){$('event').html("Sorry!")}
     
   });
-}
+
 loadTimeTable();
+
+$('#content').on('click', '#event a', function(e){
+  
+  e.preventDefault();
+  var loc = this.id.toUppperCase();
+  var newContent = '';
+  for (var i = 0; i < times[loc].length; i++){
+    newContent +=' <li><span class="time">' + times[loc][i].time + '</span>';
+    newContent += '<a href="descriptions.html#"';
+    newContent += times[loc][i].title.replace(/ /g, '-' +'">');
+    newContent += times[loc][i].title + '</a></li>';
+  }
+});
 // Click on a session to load the description
 
 $('#content').on('click', '#sessions il a', function(e){
@@ -32,8 +45,15 @@ $('#content').on('click', '#sessions il a', function(e){
 
 // Click on primary navigation
 
+$('nav a').on('click', function(e){
+  e.preventDefault();
+  var url = this.href;
 
-$('#details').text('');
+$('nav a.current').removeClass('current');
+$(this).addClass('current');
+
+$('#container').remove();
+$('#content').load(url + '#container').hide().fadeIn('slow';)
 });
 
-#page414
+
