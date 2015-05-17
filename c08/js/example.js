95f42c21f3c443c407846cb7114157a3b54b1653
karagdon <karagdon@gmail.com>
2015-05-16 22:24:12 -0700

    daily code

--- c08/js/example.js
+++ c08/js/example.js
@@ -1,60 +1,72 @@
-$(function() {
-  
-  var times;
-  $.ajax ({
-    beforeSend: function(xhr){
-      if (xhr.overrideMimeType){
-        xhr.overrideMimeType("application/json");
+$(function() {                                    // When the DOM is ready
+
+  var times;                                      // Declare global variable
+  $.ajax({
+    beforeSend: function(xhr) {                   // Before requesting data
+      if (xhr.overrideMimeType) {                 // If supported
+        xhr.overrideMimeType("application/json"); // set MIME to prevent errors
       }
     }
   });
-});
-function loadTimeTable(){
-  $.getJSON('data/example.json').done(function(data){
-    times = data;}).fail(function(){
-      $('event').html("Sorry!")});
+
+  // FUNCTION THAT COLLECTS DATA FROM THE JSON FILE
+  function loadTimetable() {                    // Declare function
+    $.getJSON('data/example.json')              // Try to collect JSON data
+    .done( function(data){                      // If successful
+      times = data;                             // Store it in a variable
+    }).fail( function() {                       // If a problem: show message
+      $('#event').html('Sorry! We could not load the timetable at the moment');
+    });
   }
 
-loadTimeTable();
+  loadTimetable();                              // Call the function
 
-$('#content').on('click', '#event a', function(e){
-  
-  e.preventDefault();
-  var loc = this.id.toUppperCase();
-  var newContent = '';
-  for (var i = 0; i < times[loc].length; i++){
-    newContent +=' <li><span class="time">' + times[loc][i].time + '</span>';
-    newContent += '<a href="descriptions.html#"';
-    newContent += times[loc][i].title.replace(/ /g, '-' +'">');
-    newContent += times[loc][i].title + '</a></li>';
-  }
+
+  // CLICK ON THE EVENT TO LOAD A TIMETABLE 
+  $('#content').on('click', '#event a', function(e) {  // User clicks on event
+
+    e.preventDefault();                                // Prevent loading page
+    var loc = this.id.toUpperCase();                   // Get value of id attr
+
+    var newContent = '';                               // Build up timetable by
+    for (var i = 0; i < times[loc].length; i++) {      // looping through events
+      newContent += '<li><span class="time">' + times[loc][i].time + '</span>';
+      newContent += '<a href="descriptions.html#';
+      newContent += times[loc][i].title.replace(/ /g, '-') + '">';
+      newContent += times[loc][i].title + '</a></li>';
+    }
+
+    $('#sessions').html('<ul>' + newContent + '</ul>'); // Display times on page
+
+    $('#event a.current').removeClass('current');       // Update selected item
+    $(this).addClass('current');
+
+    $('#details').text('');                             // Clear third column
+  });
+
+  // CLICK ON A SESSION TO LOAD THE DESCRIPTION
+  $('#content').on('click', '#sessions li a', function(e) { // Click on session
+    e.preventDefault();                                     // Prevent loading
+    var fragment = this.href;                               // Title is in href
+
+    fragment = fragment.replace('#', ' #');                 // Add space after#
+    $('#details').load(fragment);                           // To load info
+
+    $('#sessions a.current').removeClass('current');        // Update selected
+    $(this).addClass('current');
+  });
+
+
+  // CLICK ON PRIMARY NAVIGATION
+  $('nav a').on('click', function(e) {                       // Click on nav
+    e.preventDefault();                                      // Prevent loading
+    var url = this.href;                                     // Get URL to load
+
+    $('nav a.current').removeClass('current');               // Update nav
+    $(this).addClass('current');
+
+    $('#container').remove();                                // Remove old part
+    $('#content').load(url + ' #container').hide().fadeIn('slow'); // Add new
+  });
+
 });
-// Click on a session to load the description
-
-$('#content').on('click', '#sessions il a', function(e){
-  e.preventDefault();
-  var fragment = this.href;
-
-  fragment = fragment.replace('#', ' #');
-  $('#details').load(fragment);
-
-  // Update selected snippet
-  $('#sessions a.current').removeClass('current');
-  $('this').addClass('current');
-});
-
-
-// Click on primary navigation
-
-$('nav a').on('click', function(e){
-  e.preventDefault();
-  var url = this.href;
-
-$('nav a.current').removeClass('current');
-$(this).addClass('current');
-
-$('#container').remove();
-$('#content').load(url + '#container').hide().fadeIn('slow');
-});
-
-
