3cf4950ced1b9d6f74a92317b60bdfed87c52939
karagdon <karagdon@gmail.com>
2015-05-17 23:05:16 -0700

    daily code

---
+++ c09/js/geolocation2.js
@@ -0,0 +1,33 @@
+// TODO: Is geolocation supported + enabled?
+//TODO: Ask for permission to share their location
+//  TODO: IF YES, WRITE the location
+//  TODO: IF NO, WRITE not found message
+// TODO: If location not gained (for any reason), cannot be found, log to //
+// cosole
+ 
+var elMap = document.getElementById('loc');
+var msg = 'I don\'t know where you are\! :(';
+
+if (Modernizr.geolocation){
+  navigator.geolocation.getCurrentPosition(success, fail);
+  elMap.textContent = "Where am i...?";
+}
+else {
+  elMap.textContent = msg;
+}
+
+$("#loc").on('click', function(){
+
+  function success(position){
+    msg =  '<h3>Longitude:</h3>' + position.coords.longitude + '<br/><h3>Latitude:</h3>' + position.coords.latitude + '';
+    elMap.innerHTML = msg;
+    
+  }
+  
+  function fail(msg){
+    elMap.textContent = msg;
+    console.log(msg.code);
+  }
+
+
+});
