8cddfad1c330cd9b734311a43c97a5a0cc912b34
karagdon <karagdon@gmail.com>
2015-05-19 23:54:55 -0700

    daily code

---
+++ c09/js/local-storage.js
@@ -0,0 +1,17 @@
+if (window.localstorage){
+var txtUsername = document.getElementById('username');
+var txtAnswer = document.getElementById('answer');
+
+txtUserName.value = localStorage.getItem('username');
+txtAnswer.value = localStorage.getItem('answer');
+
+txtUserName.addEventListener('input', function(){
+localStorage.setItem('username', txtUserName.value)
+});
+
+txtAnswer.addEventListener('input', function(){
+localStorage.setItem('answer',txtUserName.value)
+ txtAnswer.value});
+
+
+}
