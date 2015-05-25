63b3fdd7a1c5a656dd9f88fc76f969738143d65a
karagdon <karagdon@gmail.com>
2015-05-24 23:01:17 -0700

    daily code

--- c09/js/history.js
+++ c09/js/history.js
@@ -12,6 +12,7 @@ $(function(){
   function loadContent(url){
     $('#content').load(url + '#container').hide().fadeIn('slow');
   }
+  
   $('nav a').on('click', function(e){
     e.preventDefault();
     var href = this.href;
@@ -21,4 +22,12 @@ $(function(){
     loadContent(href);
     history.pushState('', $this.text, href);
   });
+
+window.onpopstate = function(){
+  var path = location.pathname;
+  loadContent(path);
+  var page = path.substring(location.pathname.lastIndexOf("/") + 1);
+  $('a').removeClass('current');
+  $('[href="' + page + '"]').addClass('current');
+};
 });
